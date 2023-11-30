159c3064591ea30d6ce62e64e4ec3db32f7cd54b
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2023-11-30 14:51:19 +0700

    Add Custom icon

---
+++ src/components/CustomIcon.ts
@@ -0,0 +1,3 @@
+import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
+import icoMooConfig from '../../selection.json';
+export default createIconSetFromIcoMoon(icoMooConfig);
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+import CustomIcon from './CustomIcon';
+export {CustomIcon};
