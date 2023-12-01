09556555ae94ebf4a1f3ebc4f10a7b02e91550d0
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2023-12-01 15:21:38 +0700

    +Revert home

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Ionicons name="albums" size={50} />
+      <Text>Home Screen</Text>
     </View>
   );
 };
